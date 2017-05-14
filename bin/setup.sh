46d893e52911f10a00f73be7dd28b3c40b32a56b
James Klein <kleinjm007@gmail.com>
2017-05-14 13:04:44 -0400

    iterm dir set to dotfile folder

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,5 +5,4 @@ ln -s ~/GitHubRepos/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/GitHubRepos/dotfiles/gitignore_global ~/.gitignore_global
 ln -s ~/GitHubRepos/dotfiles/zshrc ~/.zshrc
 ln -s ~/GitHubRepos/dotfiles/pryrc ~/.pryrc
-ln -s ~/GitHubRepos/dotfiles/com.googlecode.iterm2.plist ~/com.googlecode.iterm2.plist
 ln -s ~/GitHubRepos/dotfiles/ssh_config ~/.ssh/config

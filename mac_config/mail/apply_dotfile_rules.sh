1a2bdf9a088d8c8558e5138e7ee62a43096fd257
James Klein <kleinjm007@gmail.com>
2017-09-25 12:47:46 -0400

    script for syncing mail rules

---
+++ mac_config/mail/apply_dotfile_rules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm ~/Library/Mail/V4/MailData/SyncedRules.plist
+cp ./mac_config/mail/SyncedRules.plist ~/Library/Mail/V4/MailData/SyncedRules.plist
---
+++ mac_config/mail/upload_local_rules.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp ~/Library/Mail/V4/MailData/SyncedRules.plist ./mac_config/mail/SyncedRules.plist

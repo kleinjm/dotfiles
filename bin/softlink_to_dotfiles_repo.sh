bdabb99d0b842c26d7a252b0d99f61acf21cfc33
James Klein <kleinjm007@gmail.com>
2017-09-25 12:37:43 -0400

    add mail rules to dotfiles

--- bin/softlink_to_dotfiles_repo.sh
+++ bin/softlink_to_dotfiles_repo.sh
@@ -7,6 +7,7 @@ rm ~/.zshrc
 rm ~/.pryrc
 rm ~/.ssh/config
 rm -r ~/.tmuxinator
+rm ~/Library/Mail/V4/MailData/SyncedRules.plist
 
 ln -s ~/GitHubRepos/dotfiles/vimrc ~/.vimrc
 ln -s ~/GitHubRepos/dotfiles/tmux.conf ~/.tmux.conf
@@ -16,3 +17,4 @@ ln -s ~/GitHubRepos/dotfiles/zshrc ~/.zshrc
 ln -s ~/GitHubRepos/dotfiles/pryrc ~/.pryrc
 ln -s ~/GitHubRepos/dotfiles/ssh_config ~/.ssh/config
 ln -s ~/GitHubRepos/dotfiles/tmuxinator ~/.tmuxinator
+ln -s ~/GitHubRepos/dotfiles/mac_config/SyncedRules.plist ~/Library/Mail/V4/MailData/SyncedRules.plist

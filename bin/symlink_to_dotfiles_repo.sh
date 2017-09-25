d25611dd754e0d05d27ae3e5eaf61b9640d3d9c0
James Klein <kleinjm007@gmail.com>
2017-09-25 13:18:11 -0400

    update symlink

--- bin/symlink_to_dotfiles_repo.sh
+++ bin/symlink_to_dotfiles_repo.sh
@@ -7,7 +7,6 @@ rm ~/.zshrc
 rm ~/.pryrc
 rm ~/.ssh/config
 rm -r ~/.tmuxinator
-rm ~/Library/Mail/V4/MailData/SyncedRules.plist
 
 ln -s ~/GitHubRepos/dotfiles/vimrc ~/.vimrc
 ln -s ~/GitHubRepos/dotfiles/tmux.conf ~/.tmux.conf
@@ -17,4 +16,3 @@ ln -s ~/GitHubRepos/dotfiles/zshrc ~/.zshrc
 ln -s ~/GitHubRepos/dotfiles/pryrc ~/.pryrc
 ln -s ~/GitHubRepos/dotfiles/ssh_config ~/.ssh/config
 ln -s ~/GitHubRepos/dotfiles/tmuxinator ~/.tmuxinator
-ln -s ~/GitHubRepos/dotfiles/mac_config/SyncedRules.plist ~/Library/Mail/V4/MailData/SyncedRules.plist

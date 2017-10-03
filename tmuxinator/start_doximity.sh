eb7f27147e8ee0ef95d5d9c30570e5988b303929
James Klein <kleinjm007@gmail.com>
2017-10-03 16:52:38 -0400

    fix script

--- tmuxinator/start_doximity.sh
+++ tmuxinator/start_doximity.sh
@@ -4,21 +4,10 @@
 tmux
 
 # a lot of apps rely on the docker container in doximity so keep it at top
-tmuxinator start doximity
-tmux detach
-
 tmuxinator start activities
-tmux detach
 tmuxinator start colleagues
-tmux detach
 tmuxinator start email-delivery
-tmux detach
 tmuxinator start residency
-tmux detach
 tmuxinator start vue-client
-tmux detach
 
 tmuxinator start dotfiles
-tmux detach
-
-tmux attach -t doximity

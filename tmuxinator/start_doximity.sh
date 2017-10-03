bab5748c94eef79f7c39dfaf1b3ffc33a79780b3
James Klein <kleinjm007@gmail.com>
2017-10-03 16:56:30 -0400

    detach stuff

--- tmuxinator/start_doximity.sh
+++ tmuxinator/start_doximity.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# start tmux
-tmux
-
 # a lot of apps rely on the docker container in doximity so keep it at top
 tmuxinator start activities
 tmuxinator start colleagues
 tmuxinator start email-delivery
 tmuxinator start residency
 tmuxinator start vue-client
-
 tmuxinator start dotfiles
+
+tmuxinator start doximity

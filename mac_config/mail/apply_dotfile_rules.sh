372190bbdd98391241c248fd5c32014c5c17b5e7
James Klein <kleinjm007@gmail.com>
2017-11-13 22:42:10 -0500

    fix upload script

--- mac_config/mail/apply_dotfile_rules.sh
+++ mac_config/mail/apply_dotfile_rules.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm ~/Library/Mail/V4/MailData/SyncedRules.plist
-cp ./mac_config/mail/SyncedRules.plist ~/Library/Mail/V4/MailData/SyncedRules.plist
+rm ~/Library/Mail/V5/MailData/SyncedRules.plist
+cp ./mac_config/mail/SyncedRules.plist ~/Library/Mail/V5/MailData/SyncedRules.plist
--- mac_config/mail/upload_local_rules.sh
+++ mac_config/mail/upload_local_rules.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp ~/Library/Mail/V4/MailData/SyncedRules.plist ./mac_config/mail/SyncedRules.plist
+cp ~/Library/Mail/V5/MailData/SyncedRules.plist ./mac_config/mail/SyncedRules.plist

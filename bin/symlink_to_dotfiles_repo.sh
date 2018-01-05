b12d999fceeaebf5511dd2e5014a1177ea4241ac
James Klein <kleinjm007@gmail.com>
2018-01-05 16:53:07 -0500

    nvm working when switching dirs

--- bin/symlink_to_dotfiles_repo.sh
+++ bin/symlink_to_dotfiles_repo.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-rm ~/.vimrc
-rm ~/.tmux.conf
+rm -r ~/.tmuxinator
 rm ~/.gitconfig
 rm ~/.gitignore_global
-rm ~/.zshrc
+rm -f ~/.nvmrc # don't fail if file doesn't exist
 rm ~/.pryrc
 rm ~/.ssh/config
-rm -r ~/.tmuxinator
+rm ~/.tmux.conf
+rm ~/.vimrc
+rm ~/.zshrc
 
-ln -s ~/GitHubRepos/dotfiles/vimrc ~/.vimrc
-ln -s ~/GitHubRepos/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/GitHubRepos/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/GitHubRepos/dotfiles/gitignore_global ~/.gitignore_global
-ln -s ~/GitHubRepos/dotfiles/zshrc ~/.zshrc
+ln -s ~/GitHubRepos/dotfiles/nvmrc ~/.nvmrc
 ln -s ~/GitHubRepos/dotfiles/pryrc ~/.pryrc
 ln -s ~/GitHubRepos/dotfiles/ssh_config ~/.ssh/config
+ln -s ~/GitHubRepos/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/GitHubRepos/dotfiles/tmuxinator ~/.tmuxinator
+ln -s ~/GitHubRepos/dotfiles/vimrc ~/.vimrc
+ln -s ~/GitHubRepos/dotfiles/zshrc ~/.zshrc

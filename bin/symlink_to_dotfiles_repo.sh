350b741e2a50005fbda19134181076043a2f9efd
James Klein <kleinjm007@gmail.com>
2018-02-16 11:04:46 -0500

    ssh config specific to env

--- bin/symlink_to_dotfiles_repo.sh
+++ bin/symlink_to_dotfiles_repo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='mac'
+fi
+
 rm -f ~/.nvmrc # don't fail if file doesn't exist
 rm -r ~/.tmuxinator
 rm ~/.gitconfig
@@ -13,7 +22,13 @@ ln -s `pwd`/gitconfig ~/.gitconfig
 ln -s `pwd`/gitignore_global ~/.gitignore_global
 ln -s `pwd`/nvmrc ~/.nvmrc
 ln -s `pwd`/pryrc ~/.pryrc
-ln -s `pwd`/ssh_config ~/.ssh/config
+
+if [ $platform = 'mac' ]; then
+  ln -s `pwd`/ssh_config_mac ~/.ssh/config
+elif [ $platform = 'linux' ]; then
+  ln -s `pwd`/ssh_config_linux ~/.ssh/config
+fi
+
 ln -s `pwd`/tmux.conf ~/.tmux.conf
 ln -s `pwd`/tmuxinator ~/.tmuxinator
 ln -s `pwd`/vimrc ~/.vimrc

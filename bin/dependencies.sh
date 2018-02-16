a401ae82ccc66f554c684fb2f0752279c5c6d2be
James Klein <kleinjm007@gmail.com>
2018-02-16 10:11:21 -0500

    linuxbrew

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# get the os version, either mac or linux. Ie. -o mac
 while getopts o: option
 do
  case "${option}"
@@ -15,53 +16,48 @@ if [ $OS = 'mac' ]; then
   sudo xcodebuild -license accept
   xcode-select --install
 
-  # brew
-  brew install bash
-  brew install fzf
-  brew install gpg                                # for github verified commits
-  brew install heroku
   brew install macvim --with-override-system-vim
-  brew install rbenv
-  brew install reattach-to-user-namespace
-  brew install the_silver_searcher
-  brew install thefuck
-
-  # make sure python is installed so we have pip
-  brew install python3
-  # Aliased to `speed` in zshrc
-  pip3 install speedtest-cli
-
-  # Needed for nerd fonts and devicons
-  # https://github.com/ryanoasis/nerd-fonts#option-3-install-script
-  brew tap caskroom/fonts
-  brew cask install font-hack-nerd-font
-  brew cask install font-devicons
-
-  # ruby
-  gem install bundler
-  cd ${PWD}/bin
-  bundle
-
-  # nvm and npm
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-  # get the latest version with "node"
-  nvm install node
-  npm install -g sass-lint
-  npm install -g coffeelint
 elif [ $OS = 'linux' ]; then
   echo 'Installing dependencies for linux...'
 
-  # ruby
-  gem install bundler
-  cd ${PWD}/bin
-  bundle
-
-  # nvm and npm
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-  # get the latest version with "node"
-  nvm install node
-  npm install -g sass-lint
-  npm install -g coffeelint
+  # linuxbrew
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+  test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+  test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+  echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
 else
   echo 'Please enter a valid os name with option -o'
 fi
+
+brew install bash
+brew install fzf
+brew install gpg                                # for github verified commits
+brew install heroku
+brew install rbenv
+brew install reattach-to-user-namespace
+brew install the_silver_searcher
+brew install thefuck
+
+# make sure python is installed so we have pip
+brew install python3
+# Aliased to `speed` in zshrc
+pip3 install speedtest-cli
+
+# Needed for nerd fonts and devicons
+# https://github.com/ryanoasis/nerd-fonts#option-3-install-script
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+brew cask install font-devicons
+
+# ruby
+gem install bundler
+cd ${PWD}/bin
+bundle
+
+# nvm and npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+# get the latest version with "node"
+nvm install node
+npm install -g sass-lint
+npm install -g coffeelint

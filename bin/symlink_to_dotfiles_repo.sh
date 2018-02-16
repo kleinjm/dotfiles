0d8ebb59a3a5a703220268aeea168e80fce8acc1
James Klein <kleinjm007@gmail.com>
2018-02-16 09:22:20 -0500

    make symfile relative

--- bin/symlink_to_dotfiles_repo.sh
+++ bin/symlink_to_dotfiles_repo.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
+rm -f ~/.nvmrc # don't fail if file doesn't exist
 rm -r ~/.tmuxinator
 rm ~/.gitconfig
 rm ~/.gitignore_global
-rm -f ~/.nvmrc # don't fail if file doesn't exist
 rm ~/.pryrc
 rm ~/.ssh/config
 rm ~/.tmux.conf
 rm ~/.vimrc
 rm ~/.zshrc
 
-ln -s ~/GitHubRepos/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/GitHubRepos/dotfiles/gitignore_global ~/.gitignore_global
-ln -s ~/GitHubRepos/dotfiles/nvmrc ~/.nvmrc
-ln -s ~/GitHubRepos/dotfiles/pryrc ~/.pryrc
-ln -s ~/GitHubRepos/dotfiles/ssh_config ~/.ssh/config
-ln -s ~/GitHubRepos/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/GitHubRepos/dotfiles/tmuxinator ~/.tmuxinator
-ln -s ~/GitHubRepos/dotfiles/vimrc ~/.vimrc
-ln -s ~/GitHubRepos/dotfiles/zshrc ~/.zshrc
+ln -s `pwd`/gitconfig ~/.gitconfig
+ln -s `pwd`/gitignore_global ~/.gitignore_global
+ln -s `pwd`/nvmrc ~/.nvmrc
+ln -s `pwd`/pryrc ~/.pryrc
+ln -s `pwd`/ssh_config ~/.ssh/config
+ln -s `pwd`/tmux.conf ~/.tmux.conf
+ln -s `pwd`/tmuxinator ~/.tmuxinator
+ln -s `pwd`/vimrc ~/.vimrc
+ln -s `pwd`/zshrc ~/.zshrc

06a76e70a71a28f3fa8373bef125a1009add72e4
James Klein <kleinjm007@gmail.com>
2018-02-21 18:47:53 -0500

    muxintator changes

--- tmuxinator/start_doximity.sh
+++ tmuxinator/start_doximity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # a lot of apps rely on the docker container in doximity so keep it at top
-apps=(doximity activities colleagues email-delivery residency vue-client pages doxbot dox-style dotfiles)
+apps=(doximity activities colleagues dotfiles)
 
 for app in ${apps[@]}; do
   echo "Starting $app"

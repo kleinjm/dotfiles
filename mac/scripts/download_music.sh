5bf368dd0169597e2aa40a99d05f60d64e610bda
James Klein <kleinjm007@gmail.com>
2018-04-18 16:09:38 -0400

    download music sync working with deletion

--- mac/scripts/download_music.sh
+++ mac/scripts/download_music.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# I think this works but I'm not positive. I would run without --delete when
-# testing
-rsync -avh jamesmkl@jamesmklein.com:/home1/jamesmkl/Music ~/Music/
+rsync -avh jamesmkl@jamesmklein.com:/home1/jamesmkl/Music ~/ --delete

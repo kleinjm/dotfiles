0cda98224446a6b56cb873e9baa33716dae65e04
James Klein <kleinjm007@gmail.com>
2018-05-01 11:17:49 -0400

    comment and sort

--- mac/zsh/aliases.zsh
+++ mac/zsh/aliases.zsh
@@ -4,13 +4,12 @@
 # Don't forget that zsh plugins like git include their own aliases
 
 # shell commands
-# TODO: some of these don't seem to work
 alias cp="cp -iv" # -i will ask form confirmation when overwriting files
-alias rm="rm -iv" # -i will ask confirmation before deleting a file
-alias mv="mv -iv" # -i will ask confirmation before overwriting an existing dir
-alias ls="ls -FGhla" # -F symbols, -G colorized output, -h full unit (Kilobyte)
-alias du="du -cksh" # disk usage
 alias df="df -h" # disk free space
+alias du="du -cksh" # disk usage
+alias ls="ls -FGhla" # TODO: fix this alias. -F symbols, -G colorized output, -h full unit (Kilobyte)
+alias mv="mv -iv" # -i will ask confirmation before overwriting an existing dir
+alias rm="rm -iv" # -i will ask confirmation before deleting a file
 
 # Use modern regexps for sed, ie. "(one|two)", not "\(one\|two\)"
 alias sed="sed -E"

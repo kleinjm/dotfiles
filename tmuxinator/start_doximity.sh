fffe05cf56d69911b500c4567d67e64a935b2d8f
James Klein <kleinjm007@gmail.com>
2018-06-04 09:11:51 -0400

    update doxstart

--- tmuxinator/start_doximity.sh
+++ tmuxinator/start_doximity.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 # a lot of apps rely on the docker container in doximity so keep it at top
-apps=(doximity doc-news dotfiles)
+apps=(doximity activities dotfiles)
 
 for app in ${apps[@]}; do
   echo "Starting $app"

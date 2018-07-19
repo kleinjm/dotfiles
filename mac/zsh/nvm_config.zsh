cbd28ce68c5f72bff20cfbfa4eca143d728047ca
James Klein <kleinjm007@gmail.com>
2018-07-19 10:43:36 -0400

    attempt to fix nvm init

--- mac/zsh/nvm_config.zsh
+++ mac/zsh/nvm_config.zsh
@@ -4,7 +4,7 @@ export NVM_DIR="$HOME/.nvm"
 # Defer initialization of nvm until nvm, node or a node-dependent command is
 # run. Ensure this block is only run once if .bashrc gets sourced multiple times
 # by checking whether __init_nvm is a function.
-if [ -s "$NVM_DIR/nvm.sh" ] && [ ! "$(whence -w __init_nvm)" = function ]; then
+if [ -s "$NVM_DIR/nvm.sh" ] && ! type "$__init_nvm" > /dev/null; then
   [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
   declare -a __node_commands=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack')
   function __init_nvm() {

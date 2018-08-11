14c4f391ba23b2255ea7936a2c0f2b43d4667b09
James Klein <kleinjm007@gmail.com>
2018-08-10 18:12:02 -0700

    sync itunes playlists

--- mac/scripts/download_music.sh
+++ mac/scripts/download_music.sh
@@ -3,4 +3,9 @@
 set -e
 set -o pipefail
 
+# copy all files in the diff and delete any remotely deleted files
 rsync -avh jamesmkl@jamesmklein.com:/home1/jamesmkl/Music ~/ --delete
+
+# copy over the library file in order to update playlists
+scp jamesmkl@jamesmklein.com:"/home1/jamesmkl/Music/iTunes/iTunes\\
+Library.itl" ~/Music/iTunes/iTunes\ Library.itl

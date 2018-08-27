3c11dc0aa477d0dab81a304e2c37d814d04db96a
James Klein <kleinjm007@gmail.com>
2018-08-27 09:32:39 -0400

    add doc news to doxstart

--- tmuxinator/start_doximity.sh
+++ tmuxinator/start_doximity.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 # a lot of apps rely on the docker container in doximity so keep it at top
-apps=(doximity activities dotfiles)
+apps=(doximity activities doc-news dotfiles)
 
 for app in ${apps[@]}; do
   echo "Starting $app"

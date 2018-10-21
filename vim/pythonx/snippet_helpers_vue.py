f27fbaa88e894ac233c260f8add41acff35bf2b2
James Klein <kleinjm007@gmail.com>
2018-10-21 14:22:45 -0400

    vue pages and components templates

--- vim/pythonx/snippet_helpers_vue.py
+++ vim/pythonx/snippet_helpers_vue.py
@@ -1,7 +1,11 @@
 from snippet_helpers import *
 import re
 
-def vue_component_name(path, snip):
+def vue_page_name(path, snip):
     path = path.split('pages/')[1]
     path = path_without_extension(path)
+    snip.rv = path_as_class_name(path, separator="")
+
+def vue_component_name(path, snip):
+    path = filename_without_extension(path)
     snip.rv = path_as_class_name(path, separator="")

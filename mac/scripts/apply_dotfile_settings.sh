de51f841fe187fb3ddf7f1352b19d159b53fbcbe
James Klein <kleinjm007@gmail.com>
2018-12-14 16:48:07 -0500

    add sql pro settings

--- mac/scripts/apply_dotfile_settings.sh
+++ mac/scripts/apply_dotfile_settings.sh
@@ -3,7 +3,9 @@
 set -e
 set -o pipefail
 
-# NOTE I tried doing this as a symlink but Mail overwrites the file
+# NOTE: I tried doing these as symlinks but Mail and other apps overwrite the
+# files.
+
 # mail rules
 rm ~/Library/Mail/V6/MailData/SyncedRules.plist
 cp ./mac/settings/SyncedRules.plist ~/Library/Mail/V6/MailData/SyncedRules.plist
@@ -11,3 +13,7 @@ cp ./mac/settings/SyncedRules.plist ~/Library/Mail/V6/MailData/SyncedRules.plist
 # keyboard settings
 rm ~/Library/Preferences/.GlobalPreferences.plist
 cp ./mac/settings/GlobalPreferences.plist ~/Library/Preferences/.GlobalPreferences.plist
+
+# sequel pro settings
+rm ~/Library/Preferences/com.sequelpro.SequelPro.plist
+cp ./sqlpro/com.sequelpro.SequelPro.plist ~/Library/Preferences/com.sequelpro.SequelPro.plist
--- mac/scripts/backup_local_settings.sh
+++ mac/scripts/backup_local_settings.sh
@@ -3,8 +3,14 @@
 set -e
 set -o pipefail
 
-# NOTE I tried doing this as a symlink but Mail overwrites the file
+# NOTE: I tried doing these as symlinks but Mail and other apps overwrite the
+# files.
+
 # mail rules
 cp ~/Library/Mail/V6/MailData/SyncedRules.plist ./mac/settings/SyncedRules.plist
+
 # keyboard settings
 cp ~/Library/Preferences/.GlobalPreferences.plist ./mac/settings/GlobalPreferences.plist
+
+# sequel pro settings
+cp ~/Library/Preferences/com.sequelpro.SequelPro.plist ./sqlpro/com.sequelpro.SequelPro.plist

354366560f49e566dff10fc598539966819d7387
James Klein <kleinjm007@gmail.com>
2018-12-14 10:45:05 -0500

    update dotfile settings mac mail version

--- mac/scripts/apply_dotfile_settings.sh
+++ mac/scripts/apply_dotfile_settings.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # NOTE I tried doing this as a symlink but Mail overwrites the file
 # mail rules
 rm ~/Library/Mail/V6/MailData/SyncedRules.plist
-cp ./mac/settings/SyncedRules.plist ~/Library/Mail/V5/MailData/SyncedRules.plist
+cp ./mac/settings/SyncedRules.plist ~/Library/Mail/V6/MailData/SyncedRules.plist
 
 # keyboard settings
 rm ~/Library/Preferences/.GlobalPreferences.plist

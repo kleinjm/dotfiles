aed5715ea0498aa74d9cc73ada37beb517bcb3f6
James Klein <kleinjm007@gmail.com>
2018-12-17 11:11:51 -0500

    fix backup and apply scripts

--- mac/scripts/apply_dotfile_settings.sh
+++ mac/scripts/apply_dotfile_settings.sh
@@ -7,7 +7,7 @@ set -o pipefail
 # files.
 
 DOTFILES_PREFERENCES_PATH=./mac/Preferences
-MAC_PREFERENCES_PATH=./mac/Preferences
+MAC_PREFERENCES_PATH=$HOME/Library/Preferences
 
 # mail rules
 rm ~/Library/Mail/V6/MailData/SyncedRules.plist
@@ -15,7 +15,7 @@ cp $DOTFILES_PREFERENCES_PATH/SyncedRules.plist ~/Library/Mail/V6/MailData/Synce
 
 # keyboard settings
 rm $MAC_PREFERENCES_PATH/.GlobalPreferences.plist
-cp $DOTFILES_PREFERENCES_PATH/GlobalPreferences.plist $MAC_PREFERENCES_PATH/.GlobalPreferences.plist
+cp $DOTFILES_PREFERENCES_PATH/.GlobalPreferences.plist $MAC_PREFERENCES_PATH/.GlobalPreferences.plist
 
 # sequel pro settings
 rm $MAC_PREFERENCES_PATH/com.sequelpro.SequelPro.plist
--- mac/scripts/backup_local_settings.sh
+++ mac/scripts/backup_local_settings.sh
@@ -7,13 +7,13 @@ set -o pipefail
 # files.
 
 DOTFILES_PREFERENCES_PATH=./mac/Preferences
-MAC_PREFERENCES_PATH=./mac/Preferences
+MAC_PREFERENCES_PATH=$HOME/Library/Preferences
 
 # mail rules
 cp ~/Library/Mail/V6/MailData/SyncedRules.plist $DOTFILES_PREFERENCES_PATH/SyncedRules.plist
 
 # keyboard settings
-cp $MAC_PREFERENCES_PATH/.GlobalPreferences.plist $DOTFILES_PREFERENCES_PATH/GlobalPreferences.plist
+cp $MAC_PREFERENCES_PATH/.GlobalPreferences.plist $DOTFILES_PREFERENCES_PATH/.GlobalPreferences.plist
 
 # sequel pro settings
 cp $MAC_PREFERENCES_PATH/com.sequelpro.SequelPro.plist $DOTFILES_PREFERENCES_PATH/com.sequelpro.SequelPro.plist

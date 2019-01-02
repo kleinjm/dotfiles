0b92270d7f4b6d1c3360095b84e9d159731cbf4f
James Klein <kleinjm007@gmail.com>
2019-01-02 13:07:05 -0500

    back up sequel pro favorites

--- mac/scripts/apply_dotfile_settings.sh
+++ mac/scripts/apply_dotfile_settings.sh
@@ -6,11 +6,11 @@ set -o pipefail
 # NOTE: I tried doing these as symlinks but Mail and other apps overwrite the
 # files.
 
-DOTFILES_PREFERENCES_PATH=./mac/Preferences
+DOTFILES_PREFERENCES_PATH=$PRIVATE_CONFIGS_DIR/mac/Preferences
 MAC_PREFERENCES_PATH=$HOME/Library/Preferences
 
 # mail rules
-rm ~/Library/Mail/V6/MailData/SyncedRules.plist
+rm $HOME/Library/Mail/V6/MailData/SyncedRules.plist
 cp $DOTFILES_PREFERENCES_PATH/SyncedRules.plist ~/Library/Mail/V6/MailData/SyncedRules.plist
 
 # keyboard settings
@@ -20,3 +20,7 @@ cp $DOTFILES_PREFERENCES_PATH/.GlobalPreferences.plist $MAC_PREFERENCES_PATH/.Gl
 # sequel pro settings
 rm $MAC_PREFERENCES_PATH/com.sequelpro.SequelPro.plist
 cp $DOTFILES_PREFERENCES_PATH/com.sequelpro.SequelPro.plist $MAC_PREFERENCES_PATH/com.sequelpro.SequelPro.plist
+
+# sequel pro favorites
+rm $HOME/Library/Application\ Support/Sequel\ Pro/Data/Favorites.plist
+cp $DOTFILES_PREFERENCES_PATH/Favorites.plist $HOME/Library/Application\ Support/Sequel\ Pro/Data/Favorites.plist
--- mac/scripts/backup_local_settings.sh
+++ mac/scripts/backup_local_settings.sh
@@ -6,14 +6,17 @@ set -o pipefail
 # NOTE: I tried doing these as symlinks but Mail and other apps overwrite the
 # files.
 
-DOTFILES_PREFERENCES_PATH=./mac/Preferences
+DOTFILES_PREFERENCES_PATH=$PRIVATE_CONFIGS_DIR/mac/Preferences
 MAC_PREFERENCES_PATH=$HOME/Library/Preferences
 
 # mail rules
-cp ~/Library/Mail/V6/MailData/SyncedRules.plist $DOTFILES_PREFERENCES_PATH/SyncedRules.plist
+cp $HOME/Library/Mail/V6/MailData/SyncedRules.plist $DOTFILES_PREFERENCES_PATH/SyncedRules.plist
 
 # keyboard settings
 cp $MAC_PREFERENCES_PATH/.GlobalPreferences.plist $DOTFILES_PREFERENCES_PATH/.GlobalPreferences.plist
 
 # sequel pro settings
 cp $MAC_PREFERENCES_PATH/com.sequelpro.SequelPro.plist $DOTFILES_PREFERENCES_PATH/com.sequelpro.SequelPro.plist
+
+# sequel pro favorites
+cp $HOME/Library/Application\ Support/Sequel\ Pro/Data/Favorites.plist $DOTFILES_PREFERENCES_PATH/Favorites.plist

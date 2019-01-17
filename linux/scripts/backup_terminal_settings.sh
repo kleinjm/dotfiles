5e2ae6c518b0af0bb916c21603f4fc2486ffcf23
James Klein <kleinjm007@gmail.com>
2019-01-17 15:30:37 -0500

    updated terminal settings

--- linux/scripts/backup_terminal_settings.sh
+++ linux/scripts/backup_terminal_settings.sh
@@ -3,4 +3,4 @@
 set -e
 set -o pipefail
 
-dconf dump /org/gnome/terminal/ > $DOTFILES_DIR/linux/terminal_settings.txt
+dconf dump /org/gnome/terminal/ > $DOTFILES_DIR/linux/config/terminal_settings.txt

db9a05aa87d9bc68810d53f1fd61c06135e7f091
James Klein <kleinjm007@gmail.com>
2019-01-17 21:15:32 -0500

    do not let script include deinstalled packages

--- linux/scripts/backup_apt_get.sh
+++ linux/scripts/backup_apt_get.sh
@@ -5,10 +5,16 @@ set -o pipefail
 
 CONFIG_PATH=$DOTFILES_DIR/linux/config
 
-# Taken from https://askubuntu.com/a/99151/911936
+# WARNING: ensure `deinstall`-ed packages are not included.
+# They be necessary on one system and not another.
+# If they are included, the following should remove them.
+# Taken from https://askubuntu.com/a/486026/911936
+sudo dpkg --purge `dpkg --get-selections | grep deinstall | cut -f1` 2> /dev/null || true
+
+# Backup script taken from https://askubuntu.com/a/99151/911936
 # NOTE: these lists are installed with linux/scripts/dependencies/apt_get.sh
-dpkg --get-selections > $CONFIG_PATH/Package.list
-sudo cp -R /etc/apt/sources.list* $CONFIG_PATH/
-sudo apt-key exportall > $CONFIG_PATH/Repo.keys
+dpkg --get-selections > "$CONFIG_PATH"/Package.list
+sudo cp -R /etc/apt/sources.list* "$CONFIG_PATH"/
+sudo apt-key exportall | sudo tee -a "$CONFIG_PATH"/Repo.keys > /dev/null
 
 echo "Finshed backing up apt get packages"

73f356bd38468fe49a563ac8a14ce356f1ef94f9
James Klein <kleinjm007@gmail.com>
2019-01-18 10:15:08 -0500

    install apt clone

--- linux/scripts/backup_apt_get.sh
+++ linux/scripts/backup_apt_get.sh
@@ -6,6 +6,7 @@ set -o pipefail
 : "${DOTFILES_DIR:=$HOME/GitHubRepos/dotfiles}"
 CONFIG_PATH=$DOTFILES_DIR/linux/config
 
+sudo apt-get install apt-clone
 sudo apt-clone clone "$CONFIG_PATH"/apt-clone-state-ubuntu-"$(lsb_release -sr)".tar.gz
 
 echo "Finshed backing up apt get packages"

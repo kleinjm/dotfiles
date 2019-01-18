b1045a07514f889db6f5d3493da8541ebc97a5c5
James Klein <kleinjm007@gmail.com>
2019-01-18 18:16:40 -0500

    fzf setup script

---
+++ linux/scripts/dependencies/fzf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+echo "***Installing fzf***"
+
+# See https://github.com/junegunn/fzf.vim/issues/1#issuecomment-422456830
+# The vim part of this is backed up in the dotfiles
+git clone https://github.com/junegunn/fzf $PROJECT_DIR/fzf
+cd $PROJECT_DIR/fzf
+./install
+sudo mv bin/fzf /bin

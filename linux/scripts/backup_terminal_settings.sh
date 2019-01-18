b4e4c7314939c64e77769f08930a5e67b8f1e03a
James Klein <kleinjm007@gmail.com>
2019-01-18 18:06:38 -0500

    backup gnome tweak settings

--- linux/scripts/backup_terminal_settings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-dconf dump /org/gnome/terminal/ > $DOTFILES_DIR/linux/config/terminal_settings.txt
--- linux/scripts/restore_terminal_settings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-dconf load /org/gnome/terminal/ < $DOTFILES_DIR/linux/config/terminal_settings.txt

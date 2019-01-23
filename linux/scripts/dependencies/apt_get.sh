2d9269a7dc51e7ae9cb0d3b07813a8715380a350
James Klein <kleinjm007@gmail.com>
2019-01-23 11:53:30 -0500

    albert dependency

--- linux/scripts/dependencies/apt_get.sh
+++ linux/scripts/dependencies/apt_get.sh
@@ -10,7 +10,11 @@ echo "***Installing apt-get packages***"
 # involved in the backup, they do not work between computers.
 
 sudo apt-add-repository ppa:rodsmith/refind
+
+# for albert
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
+
 sudo apt-get update
 
 # Ensure packages added here are not part of the brew bundle
-sudo apt install vim-gtk curl zsh ksshaskpass gnome-tweak-tool refind libffi-dev xclip
+sudo apt install vim-gtk curl zsh ksshaskpass gnome-tweak-tool refind libffi-dev xclip albert

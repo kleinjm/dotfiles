ad02e226eb419112f18de9b80a6758691ae0ade8
James Klein <kleinjm007@gmail.com>
2019-01-25 10:33:39 -0500

    backup and restore keyboard and other settings

--- linux/scripts/backup_settings.sh
+++ linux/scripts/backup_settings.sh
@@ -3,6 +3,8 @@
 set -e
 set -o pipefail
 
+echo "Backing up settings"
+
 CONFIG_PATH="$DOTFILES_DIR"/linux/config
 
 # terminal settings
@@ -11,3 +13,6 @@ dconf dump /org/gnome/terminal/ > "$CONFIG_PATH"/terminal_settings.txt
 # gnome tweak settings
 # See https://askubuntu.com/a/1056392
 dconf dump / > "$CONFIG_PATH"/gnome_tweaks_settings.dconf
+
+# All user settings
+dconf dump / > "$CONFIG_PATH"/user.conf
--- linux/scripts/restore_settings.sh
+++ linux/scripts/restore_settings.sh
@@ -11,3 +11,7 @@ dconf load /org/gnome/terminal/ < "$CONFIG_PATH"/terminal_settings.txt
 # gnome tweak settings
 # See https://askubuntu.com/a/1056392
 dconf load / < "$CONFIG_PATH"/gnome_tweaks_settings.dconf
+
+# All user settings
+# See https://askubuntu.com/a/746262/911936
+dconf load / < "$CONFIG_PATH"/user.conf

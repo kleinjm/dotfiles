6c32f9d760b34c1370043144b0cf431a48ef253e
James Klein <kleinjm007@gmail.com>
2019-01-25 17:33:35 -0500

    useful output

--- shared/scripts/dependencies/gems.sh
+++ shared/scripts/dependencies/gems.sh
@@ -23,6 +23,9 @@ gem install bundler # if this command fails, run `rbenv init`
 #         2: from /home/james/.rbenv/versions/2.5.3/bin/bundle:23:in `<main>'
 #         1: from /home/james/.rbenv/versions/2.5.3/lib/ruby/2.5.0/rubygems.rb:308:in `activate_bin_path'
 # /home/james/.rbenv/versions/2.5.3/lib/ruby/2.5.0/rubygems.rb:289:in `find_spec_for_exe': can't find gem bundler (>= 0.a) with executable bundle (Gem::GemNotFoundException)
+< Gemfile.lock grep -A 1 "BUNDLED WITH"
+echo "Run gem install bundler -v '...' if bundle fails"
+
 bundle
 
 gem ctags # index every rubygem installed on the system. Will run automatically after the first time

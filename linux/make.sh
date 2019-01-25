a3e4e39be1b1d28f369b3c54beba49b3d98b1dfe
James Klein <kleinjm007@gmail.com>
2019-01-25 09:26:43 -0500

    linux make file

--- linux/make.sh
+++ linux/make.sh
@@ -9,4 +9,8 @@ echo "***Setting up everything***"
 export DOTFILES_DIR=$DOTFILES_DIR
 
 "$DOTFILES_DIR"/linux/scripts/dependencies.sh
+"$DOTFILES_DIR"/linux/scripts/restore_settings.sh
 "$DOTFILES_DIR"/linux/scripts/symlink_to_dotfiles_repo.sh
+"$DOTFILES_DIR"/linux/scripts/fontconfig.sh
+
+echo "Everything set up. Check linux/scripts for any additional setup"

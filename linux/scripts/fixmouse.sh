4ec73636fbfd99335fb138b54c7e3c18b7dbadbb
James Klein <kleinjm007@gmail.com>
2019-01-25 10:26:00 -0500

    comment

--- linux/scripts/fixmouse.sh
+++ linux/scripts/fixmouse.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
+# See http://sneclacson.blogspot.com/2016/09/using-apple-magic-mouse-with-ubuntu-1604.html
 sudo rmmod hid_magicmouse
 sudo modprobe hid_magicmouse emulate_3button=0 scroll_acceleration=1 scroll_speed=45

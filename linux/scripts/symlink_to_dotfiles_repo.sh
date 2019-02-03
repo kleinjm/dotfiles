8ee91765f3121f04ee77497dd529348bc12f8ab0
James Klein <kleinjm007@gmail.com>
2019-02-02 23:43:54 -0500

    fix mac ssh symlink

--- linux/scripts/symlink_to_dotfiles_repo.sh
+++ linux/scripts/symlink_to_dotfiles_repo.sh
@@ -9,12 +9,16 @@ set -o pipefail
 stow -v -t "$HOME" -d "$PRIVATE_CONFIGS_DIR" bundle --ignore='.bundle/cache/*'
 stow -v -t "$HOME" -d "$PRIVATE_CONFIGS_DIR" aws
 stow -v -t "$HOME" -d "$PRIVATE_CONFIGS_DIR"/linux docker
+
 stow -v -t "$HOME" -d shared git
 stow -v -t "$HOME" -d linux git
+
 stow -v -t "$HOME" -d shared zsh
 stow -v -t "$HOME" -d linux zsh
+
 stow -v -t "$HOME" -d shared vim
 stow -v -t "$HOME" -d linux vim
+
 stow -v -t /etc -d linux etc # may need sudo access
 stow -v -t "$HOME" nvm
 stow -v -t "$HOME" pry
@@ -22,4 +26,9 @@ stow -v -t "$HOME" psql
 stow -v -t "$HOME" pyenv
 stow -v -t "$HOME" rbenv
 stow -v -t "$HOME" -d linux tmux
-stow -v -t "$HOME" -d "$PRIVATE_CONFIGS_DIR" ssh
+
+mkdir -p "$HOME"/.ssh
+stow -v -t "$HOME"/.ssh -d "$PRIVATE_CONFIGS_DIR"/linux/ssh .ssh
+stow -v -t "$HOME"/.ssh -d "$PRIVATE_CONFIGS_DIR"/ssh .ssh
+
+echo "Symlinking completed successfully"
--- mac/scripts/symlink_to_dotfiles_repo.sh
+++ mac/scripts/symlink_to_dotfiles_repo.sh
@@ -9,20 +9,29 @@ set -o pipefail
 stow -v -t "$HOME" -d "$PRIVATE_CONFIGS_DIR" bundle --ignore='.bundle/cache/*'
 stow -v -t "$HOME" -d "$PRIVATE_CONFIGS_DIR" aws
 stow -v -t "$HOME" -d mac docker
+
 stow -v -t "$HOME" -d shared git
 stow -v -t "$HOME" -d mac git
+
 stow -v -t "$HOME" nvm
 stow -v -t "$HOME" pry
 stow -v -t "$HOME" psql
 stow -v -t "$HOME" pyenv
 stow -v -t "$HOME" rbenv
 stow -v -t "$HOME" -d mac tmux
+
 stow -v -t "$HOME" -d shared vim
 stow -v -t "$HOME" -d mac vim
+
 # may need to `rm $HOME/.zshrc`
 stow -v -t "$HOME" -d shared zsh
 stow -v -t "$HOME" -d mac zsh
-sudo stow -v -t "$HOME" -d mac ssh
+
+mkdir -p "$HOME"/.ssh
+stow -v -t "$HOME"/.ssh -d "$PRIVATE_CONFIGS_DIR"/mac/ssh .ssh
+stow -v -t "$HOME"/.ssh -d "$PRIVATE_CONFIGS_DIR"/ssh .ssh
 
 # May need to update permissions
 # chmod -R 0755 ~/.git/git_template/hooks
+
+echo "Symlinking completed successfully"

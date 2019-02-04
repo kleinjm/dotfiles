d59d379ab6c5978cff4de54515ff0c33b74f8815
James Klein <kleinjm007@gmail.com>
2019-02-04 17:00:22 -0500

    fix webcam driver script

--- linux/scripts/mac_webcam.sh
+++ linux/scripts/mac_webcam.sh
@@ -3,10 +3,14 @@
 set -e
 set -o pipefail
 
-cd $PROJECT_DIR
-git clone https://github.com/patjak/bcwc_pcie.git
+cd "$PROJECT_DIR"
+if [ ! -d "$PROJECT_DIR"/bcwc_pcie ]; then
+  git clone https://github.com/patjak/bcwc_pcie.git
+fi
+
+cd bcwc_pcie
 git checkout mainline
-cd bcwc_pcie/firmware
+cd firmware
 make
 sudo make install
 cd ..

53d1579415a206f7f363daa5813f0229eb167bcd
James Klein <kleinjm007@gmail.com>
2019-03-23 16:32:32 -0400

    comment

--- mac/scripts/dependencies/python.sh
+++ mac/scripts/dependencies/python.sh
@@ -6,7 +6,7 @@ set -o pipefail
 echo "***Installing Python and Requirements***"
 
 # make sure python is installed so we have pip
-# NOTE: it's also a macvim dependency
+# See: https://github.com/pyenv/pyenv/issues/1219#issuecomment-428793012
 < "$DOTFILES_DIR"/pyenv/.pyenv/version pyenv install
 
 pip install -r shared/vim/.vim/pythonx/requirements.txt

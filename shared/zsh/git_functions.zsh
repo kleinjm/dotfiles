4d58679571fed6d7768b562a66ddc35372593b29
James Klein <kleinjm007@gmail.com>
2019-05-24 13:58:04 -0400

    update gprune command

--- shared/zsh/git_functions.zsh
+++ shared/zsh/git_functions.zsh
@@ -1,6 +1,10 @@
 # delete local branches that do not exist on origin
 gprune() {
-  git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
+  git fetch --prune &&
+    git branch -r |
+    awk '{print $1}' |
+    egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) |
+    awk '{print $1}' | xargs git branch -D
 }
 
 # pull the latest remote changes for this branch and merge latest remote master

ea187608de0fc3387c52003e3635bd5246c2a3d8
James Klein <kleinjm007@gmail.com>
2019-10-01 15:07:00 -0400

    update vim plugins

--- shared/scripts/dependencies/zsh_autosuggestions.sh
+++ shared/scripts/dependencies/zsh_autosuggestions.sh
@@ -7,5 +7,5 @@ echo "***Installing zsh-autosuggestions***"
 
 INSTALL_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
 if [ ! -d "$INSTALL_DIR" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions $INSTALL_DIR
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$INSTALL_DIR"
 fi

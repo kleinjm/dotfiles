efbba346124ca1644097233878f86627aa0b668c
James Klein <kleinjm007@gmail.com>
2019-11-05 09:42:03 -0500

    bump system ruby version, alias tmuxinator to latest, add plugins

--- shared/zsh/aliases.zsh
+++ shared/zsh/aliases.zsh
@@ -9,3 +9,7 @@ alias ls="ls -FGhla" # -F symbols, -G colorized output, -h full unit (Kilobyte)
 # Brew
 # Remove pyenv configs as not to interfere with brew
 alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew"
+
+# Always use the latest system ruby version for the latest tmuxinator version
+# Need to wait until rbenv is loaded
+alias mux="RBENV_VERSION=$(rbenv global) tmuxinator"

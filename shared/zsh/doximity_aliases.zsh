a52373418f7648002f2dd0789450a442f970c666
James Klein <kleinjm007@gmail.com>
2020-03-03 09:36:52 -0700

    asa ssh login

--- shared/zsh/doximity_aliases.zsh
+++ shared/zsh/doximity_aliases.zsh
@@ -1,11 +1,12 @@
+alias asa="stf login && ssh"
 alias ddbundle="dox-do bundle install"
 alias ddep="dox-do bundle install && dox-do rails db:migrate"
-alias dyarn="yarn && dox-do yarn"
 alias ddrT="dox-do rake -T | grep "
 alias ddrdm="dox-do rails db:migrate"
 alias ddrdr="dox-do rails db:rollback"
 alias ddrrg="dox-do rails routes | grep "
 alias ddsh="dox-do bash"
+alias dyarn="yarn && dox-do yarn"
 alias e2e-single="TEST_WEBDRIVER_TIMEOUT=99999999 SKIP_OAUTH=true ./node_modules/.bin/wdio --spec"
 alias pre-chamber='AWS_REGION=us-east-1 CHAMBER_KMS_KEY_ALIAS=pre-parameter-store aws-vault exec hipaa -- chamber'
 alias prod-chamber='AWS_REGION=us-east-1 CHAMBER_KMS_KEY_ALIAS=prod-parameter-store aws-vault exec hipaa -- chamber'

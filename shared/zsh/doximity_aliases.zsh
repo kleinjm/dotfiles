af6188499918c21718b46ea342740d2df6adab25
James Klein <kleinjm007@gmail.com>
2020-04-22 13:09:02 -0600

    gdep command to pull master and resolve dependencies

--- shared/zsh/doximity_aliases.zsh
+++ shared/zsh/doximity_aliases.zsh
@@ -8,6 +8,7 @@ alias ddrrg="dox-do rails routes | grep "
 alias ddsh="dox-do bash"
 alias dyarn="yarn && dox-do yarn"
 alias e2e-single="TEST_WEBDRIVER_TIMEOUT=99999999 SKIP_OAUTH=true ./node_modules/.bin/wdio --spec"
+alias gdep="git pull origin master && dox-do bundle install && dox-do rais db:migrate"
 alias pre-chamber='AWS_REGION=us-east-1 CHAMBER_KMS_KEY_ALIAS=pre-parameter-store aws-vault exec hipaa -- chamber'
 alias prod-chamber='AWS_REGION=us-east-1 CHAMBER_KMS_KEY_ALIAS=prod-parameter-store aws-vault exec hipaa -- chamber'
 alias sqlpro="sft login && open /Applications/Sequel\ Pro.app"

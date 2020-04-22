c604ad06d44a4a5ba8bc8f94381ce9069b3fc899
James Klein <kleinjm007@gmail.com>
2020-04-22 15:10:42 -0600

    fix dox alias

--- shared/zsh/doximity_aliases.zsh
+++ shared/zsh/doximity_aliases.zsh
@@ -8,8 +8,8 @@ alias ddrrg="dox-do rails routes | grep "
 alias ddsh="dox-do bash"
 alias dyarn="yarn && dox-do yarn"
 alias e2e-single="TEST_WEBDRIVER_TIMEOUT=99999999 SKIP_OAUTH=true ./node_modules/.bin/wdio --spec"
-alias gdep="git pull origin master && dox-do bundle install && dox-do rais db:migrate" # pull master into the current branch and resolve dependencies
-alias mdep="git master && dox-do bundle install && dox-do rais db:migrate" # switch to master and resolve dependencies
+alias gdep="git pull origin master && dox-do bundle install && dox-do rails db:migrate" # pull master into the current branch and resolve dependencies
+alias mdep="git master && dox-do bundle install && dox-do rails db:migrate" # switch to master and resolve dependencies
 alias pre-chamber='AWS_REGION=us-east-1 CHAMBER_KMS_KEY_ALIAS=pre-parameter-store aws-vault exec hipaa -- chamber'
 alias prod-chamber='AWS_REGION=us-east-1 CHAMBER_KMS_KEY_ALIAS=prod-parameter-store aws-vault exec hipaa -- chamber'
 alias sqlpro="sft login && open /Applications/Sequel\ Pro.app"

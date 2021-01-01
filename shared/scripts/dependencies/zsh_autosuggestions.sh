59b88acbff30506af07e375199d1bdb9622e7c45
James Klein <kleinjm007@gmail.com>
2021-01-01 18:03:09 -0500

    add vim color scheme and fix autosuggestion dependency install script

--- shared/scripts/dependencies/zsh_autosuggestions.sh
+++ shared/scripts/dependencies/zsh_autosuggestions.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 echo "***Installing zsh-autosuggestions***"
 
-INSTALL_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
+INSTALL_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 if [ ! -d "$INSTALL_DIR" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions "$INSTALL_DIR"
 fi

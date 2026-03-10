dc2f73fc3bc94f2552e5272e1f6cb2763c7f3046
James Klein <kleinjm007@gmail.com>
2026-03-10 15:55:07 +0000

    worktree git support

--- devpod/functions.zsh
+++ devpod/functions.zsh
@@ -1,7 +1,9 @@
 # Git: delete local branches that don't exist on origin
 gprune() {
   git fetch --prune
-  git branch -vv | grep ': gone]' | awk '{gsub(/^[* +]+/, ""); print $1}' | xargs -r git branch -D
+  local worktree_branches=$(git worktree list --porcelain 2>/dev/null | awk '/^branch / {sub(/^refs\/heads\//, "", $2); print $2}')
+  git branch -vv | grep ': gone]' | awk '{gsub(/^[* +]+/, ""); print $1}' |
+    grep -vxF "$worktree_branches" | xargs -r git branch -D
 }
 
 # Git: pull latest and merge main
